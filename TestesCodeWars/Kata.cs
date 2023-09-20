73f33cda5b479d5746dd267bdc39bbf77011f313
João Pedro de Bem <jpdebem@gmail.com>
2023-09-20 10:43:46 -0300

    Subindo arquivos

---
+++ TestesCodeWars/Kata.cs
@@ -0,0 +1,12 @@
+using System;
+public class Kata
+{
+    public static string CreatePhoneNumber(int[] numbers)
+    {
+
+        var newNumbers = numbers.ToString();
+
+        return newNumbers;
+
+    }
+}
---
+++ TestesCodeWars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Dynamic;
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        Kata.CreatePhoneNumber(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
+
+        Console.WriteLine();
+    }
+
+
+}
+
